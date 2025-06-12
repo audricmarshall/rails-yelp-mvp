2a93ad24037d395811120725fa12ebc8cec1679c
Audric Nomentsoa <audricmarshall@gmail.com>
2025-06-12 19:18:28 +0200

    Défini les routes principales et mis en place la page d'accueil sur l'index des restaurants meme si il n'y a pas encore de view ni de controllers

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+    # pour GET new review et POST create review
+  end
+  root to: "restaurants#application"
+  # je le mets maintenant meme si je n'ai pas de view encore pour
+  # préshot la root vers l'index et éviter la page de rails
 end
