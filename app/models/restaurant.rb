a9cb1e744ab88cc5c1c489e359e42281572eb2fb
Audric Nomentsoa <audricmarshall@gmail.com>
2025-06-12 19:06:52 +0200

    génération des modèles + migrations + validations + associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,10 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  # si on supprime un resto, ça supprime aussi les reviews
+  validates :name, :address, :category, presence: true
+
+  CATEGORIES = %w[chinese italian japanese french belgian].freeze
+  # obliger de le f iger pour éviter qu'on modifie son contenu
+  validates :category, inclusion: { in: CATEGORIES }
+  # catégorie doit être dans la liste
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: 0..5 }
+  # rating entre 0 et 5 et que entier
 end
